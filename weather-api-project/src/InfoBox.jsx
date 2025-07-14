e39ea44feb55be86e10ce4dcfbcd467c8e19fea8
Rishima <rishima1711@gmail.com>
2025-07-14 15:18:36 +0530

    Update InfoBox.jsx

--- weather-api-project/src/InfoBox.jsx
+++ weather-api-project/src/InfoBox.jsx
@@ -11,8 +11,8 @@ export default function InfoBox({ info }) {
     "https://images.unsplash.com/photo-1524555259-3e4f9092f97b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   const HOT_URL =
     "https://images.unsplash.com/photo-1525490829609-d166ddb58678?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-  const COLD_URL =
-    "https://images.unsplash.com/photo-1616951849649-74dd2dd7e662?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const COLD_URL ="https://images.unsplash.com/photo-1579847641267-5445b760b991?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+ 
   const RAIN_URL =
     "https://plus.unsplash.com/premium_photo-1671229652411-4468b946b787?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
